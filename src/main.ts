0a58f3edde2f7d10ab36c39e899110bb66944763
sbaos <lop300520@gmail.com>
2025-11-15 23:07:04 +0700

    .

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // enable DTO validation
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  await app.listen(3000);
+}
+bootstrap();
